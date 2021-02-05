d9f8d8e54841db2b1cddb493ac62e38088aa7985
Stephen Doan <>
2021-02-05 14:47:56 -0800

    iterates through playlist as well as resolutions; needs network-stats implementation

--- src/generate-data.py
+++ src/generate-data.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 import time
+
+resolutions = [
+    '240p',
+    '480p'
+]
 
 PATH = "../web-drivers/chromedriver.exe"
 # driver_options = Options()
@@ -17,26 +23,64 @@ driver = webdriver.Chrome(PATH)#, options=driver_options)
 driver.get("https://youtube.com/playlist?list=PL51jetv1tnxncETYqCffQTfyOyES9YOVx")
 
 videos = driver.find_elements_by_id("video-title")
-video_links = [link.get_attribute('href') for link in videos]
+video_links = [link.get_attribute("href") for link in videos]
 
 for link in video_links:
     driver.get(link)
     print(driver.title)
+
+    ## skip ad
+
     time.sleep(10)
+    
+    try:
+        skip_ad = driver.find_element_by_class_name("ytp-ad-skip-button-container")
+        skip_ad.click()
+    
+    except NoSuchElementException:
+        pass
+
+    ## mute
+    mute_btn = driver.find_element_by_class_name("ytp-mute-button")
+    volume_status = mute_btn.get_attribute("title")
+
+    if "Mute" in volume_status:
+        mute_btn.click()
+
+    time.sleep(2)
+    print(volume_status)
+
+    ## iterate through resolutions
+
+    for curr_res in resolutions:
+        playback_settings = driver.find_element_by_class_name("ytp-settings-button")
+        playback_settings.click()
+        
+        time.sleep(2)
+        
+        driver.find_element_by_xpath("//div[contains(text(),'Quality')]").click()
+        
+        time.sleep(2)
+        
+        quality = driver.find_element_by_xpath("//span[contains(string(), '{}')]".format(curr_res))
+        quality.click()
+
+    ## get video duration
+    time.sleep(2)
+    video_dur = driver.execute_script(
+                    "return document.getElementById('movie_player').getCurrentTime()"
+                    )
+
+    video_len = driver.execute_script(
+                    "return document.getElementById('movie_player').getDuration()"
+                    )
+
+    print(video_len)
+
+    time.sleep(5)
 
 driver.close()
-## click a video
-# videos = driver.find_element_by_id("thumbnail")
-# videos.click()
-#
-# # wait & skip ad
-# time.sleep(10)
-#
-# skip_ad = driver.find_elements_by_class_name("ytp-ad-skip-button-container")
-#
-# if len(skip_ad) > 0:
-#     skip_ad[0].click()
-#
+
 # playback_settings = driver.find_element_by_class_name("ytp-settings-button")
 # playback_settings.click()
 #
