c09fae2d7de34cb1742cf233adb81a58e23c8e4f
Stephen Doan <>
2021-02-18 00:51:28 -0800

    working collection script - works better when collecting from less videos

--- src/generate-data.py
+++ src/generate-data.py
@@ -11,18 +11,17 @@ if __name__ == "__main__":
 
     generate_data_params = json.load(open("../config/generate-data-params.json"))
 
-    network_stats = generate_data_params["path"]
+    network_stats = generate_data_params["network_stats_path"]
     interface = generate_data_params["interface"]
+    playlist = generate_data_params["playlist"]
     outdir = generate_data_params["outdir"]
-    resolutions = ['240p', '1080p'] #generate_data_params["resolutions"]
+    resolutions = generate_data_params["resolutions"]
 
     PATH = "../web-drivers/chromedriver.exe"
-
     driver = webdriver.Chrome(PATH)
 
     ## go to playlist
-    driver.get("https://www.youtube.com/playlist?list=PL51jetv1tnxlzspzw_ljtr3H-vYPrvxGb")
-
+    driver.get(playlist)
     videos = driver.find_elements_by_id("video-title")
     video_links = [link.get_attribute("href") for link in videos]
 
@@ -30,20 +29,22 @@ if __name__ == "__main__":
 
     for target_res in resolutions:
 
-        for link in video_links[:2]:
+        for link in video_links:
             driver.get(link)
 
             ## skip ad
-
             ## check if ad is paused
 
             player_status = driver.execute_script("return document.getElementById('movie_player').getPlayerState()")
-            print(player_status)
+            print(player_status, flush=True)
+
+            driver.execute_script("return document.getElementById('movie_player').playVideo()")
+            print(player_status, flush=True)
+
             try:
                 video_ad = driver_wait.until(
                     EC.element_to_be_clickable((By.CLASS_NAME, "ytp-ad-skip-button-container"))
                 )
-
                 video_ad.click()
             
             except (NoSuchElementException, TimeoutException) as e:
@@ -52,13 +53,7 @@ if __name__ == "__main__":
             time.sleep(3)
 
             ## get resolution
-
-            # settings_button = driver_wait.until(
-            #     EC.element_to_be_clickable((By.CLASS_NAME, "ytp-settings-button"))
-            # )
-            # settings_button.click()
             driver.find_element_by_class_name("ytp-settings-button").click()
-
             driver.find_element_by_xpath("//div[contains(text(),'Quality')]").click()
 
             time.sleep(2)
@@ -74,8 +69,8 @@ if __name__ == "__main__":
             video_len = driver.execute_script(
                             "return document.getElementById('movie_player').getDuration()"
                         )
-
-            time.sleep(5)
+            print(video_len, flush=True)
+            time.sleep(video_len)
 
             collect_data.terminate()
 
